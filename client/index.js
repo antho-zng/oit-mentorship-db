d773230997fa0ee701625aba2973fc7f1a74972e
lateral-neck-stretch <zhang.anthony97@gmail.com>
2023-03-30 11:47:54 -0400

    switched from ReactDOM to createRoot for React 18

--- client/index.js
+++ client/index.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
-import App from './App'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import history from './history';
+import store from './store';
+import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('app'));
+root.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </Router>
-  </Provider>,
-  document.getElementById('app')
-)
+  </Provider>
+);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={history}>
+//       <App />
+//     </Router>
+//   </Provider>,
+//   document.getElementById('app')
+// );
