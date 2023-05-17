0e4e2fa1a583dcdf7aac964014862c9399c7fa1b
lateral-neck-stretch <zhang.anthony97@gmail.com>
2023-05-17 13:20:41 -0400

    fetch mentee review and scores with cookies

--- server/db/models/Review.js
+++ server/db/models/Review.js
@@ -1,26 +1,36 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-module.exports = db.define('review', {
-  reviewerComments: {
-    type: Sequelize.TEXT,
-    allowNull: true,
+module.exports = db.define(
+  'review',
+  {
+    reviewerComments: {
+      type: Sequelize.TEXT,
+      allowNull: true,
+    },
+    reviewerScore: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    },
+    // reviewerId: {
+    //   type: Sequelize.STRING,
+    //   references: {
+    //     model: 'users',
+    //     key: 'username',
+    //   },
+    // },
+    // menteeId: {
+    //   type: Sequelize.STRING,
+    //   references: {
+    //     model: 'mentees',
+    //     key: 'id',
+    //   },
+    // },
   },
-  reviewerScore: {
-    type: Sequelize.INTEGER,
-    allowNull: true,
-  },
-  // reviewerId: {
-  //   type: Sequelize.STRING,
-  //   references: {
-  //     model: 'users',
-  //     key: 'username',
-  //   },
-  // },
-  // menteeId: {
-  //   type: Sequelize.STRING,
-  //   references: {
-  //     model: 'mentees',
-  //     key: 'id',
-  //   },
-  // },
-});
+  {
+    indexes: [
+      {
+        fields: ['reviewerScore'],
+      },
+    ],
+  }
+);
